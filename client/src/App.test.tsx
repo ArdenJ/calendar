56d1ff5452fed7f078f6a5ad78f6651cf9e50288
ArdenJ <j.gaddas@outlook.com>
2020-02-11 11:26:41 +0000

    created test on initial render
    test checks that a welcome message is displayed as opposed to a blank summary component

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import App from './App'
 import moment from 'moment'
 
@@ -11,4 +11,10 @@ test('it renders the correct month on load', () => {
   const { getByText } = render(<App />)
   const presentMonth = getByText(month)
   expect(presentMonth).toBeInTheDocument()
+})
+
+test('the summary displays a welcome message', () => {
+  const { getByText } = render(<App />)
+  const welcomeMessage = getByText('Hello')
+  expect(welcomeMessage).toBeInTheDocument()
 })
