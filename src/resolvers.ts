d7cc475ec466350b95ad58a6a29a8bab6229d72f
ArdenJ <j.gaddas@outlook.com>
2020-02-11 12:01:35 +0000

    extract resolvers and typeDefs to their own files

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,90 @@
-import { gql } from "apollo-server";
+const fetch = require('node-fetch')
 
-export const resolvers = {
+// GQL - resolvers
+const resolvers = {
   Query: {
-    ToDos: "odgij"
-  }
-};
+    events: (root, args, { db }, info) => {
+      return fetch(db)
+        .then(res => res.json())
+        .then(data => data)
+        .catch(err => console.log(err))
+    },
+    event: (root, { id }, { db }, info) => {
+      return fetch(db)
+        .then(res => res.json())
+        .then(data => data.find(i => i.id === id))
+        .then(data => data)
+        .catch(err => console.log(err))
+    },
+    eventsByMonth: (root, { date }, { db }, info) => {
+      return fetch(db)
+        .then(res => res.json())
+        .then(data =>
+          data.filter(i => i.date.substr(3, 9) === date.substr(3, 9)),
+        )
+        .then(data => data)
+        .catch(err => console.log(err))
+    },
+    eventsByDay: (root, { date }, { db }, info) => {
+      return fetch(db)
+        .then(res => res.json())
+        .then(data => data.filter(i => i.date === date))
+        .then(data => data)
+        .catch(err => console.log(err))
+    },
+  },
+
+  Mutation: {
+    // Create new EVENT
+    createEVENT: (root, { title, date, id }, { db }, info) => {
+      const newEVENT = {
+        id,
+        title,
+        date,
+      }
+      return fetch(db, {
+        method: 'POST',
+        body: JSON.stringify(newEVENT),
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then(res => res.json())
+        .then(data => data)
+        .catch(err => console.log(err))
+    },
+
+    // Update existing EVENT
+    updateEVENT: (root, args, { db }, info) => {
+      const editEVENT = {
+        ...args,
+      }
+      console.log(editEVENT)
+      return fetch(`${db}/${args.id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(editEVENT),
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          return data
+        })
+        .catch(err => console.log(err))
+    },
+
+    // Delete existing EVENT
+    // TODO: How to handle a the return - there is no data - can I return a generic?
+    deleteEVENT: (root, { id }, { db }, info) => {
+      return fetch(`${db}/${id}`, {
+        method: 'DELETE',
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          return { id, title: 'empty', date: '' }
+        })
+        .catch(err => console.log(err))
+    },
+  },
+}
+
+export default resolvers
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,13 +1,25 @@
 const { gql } = require('apollo-server')
 
-export const typeDefs = gql`
-  type ToDo {
+// GQL - TypeDefs
+const typeDefs = gql`
+  type EVENT {
     id: String!
     title: String!
     date: String!
   }
 
   type Query {
-    ToDos: [ToDo!]!
+    events: [EVENT!]!
+    eventsByMonth(date: String!): [EVENT]!
+    eventsByDay(date: String!): [EVENT]!
+    event(id: String!): EVENT!
+  }
+
+  type Mutation {
+    createEVENT(title: String!, date: String!, id: String!): EVENT!
+    updateEVENT(id: String!, title: String, date: String): EVENT!
+    deleteEVENT(id: String!): EVENT
   }
 `
+
+export default typeDefs
